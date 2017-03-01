eeabcdeedc7a21dd9d8404c6702ad91100eb3896
Silver <Silver@LAPTOP-F7OARP06>
2017-03-01 15:44:11 +0300

    Add project files.

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello word??????????????????????");
+            
+            Console.ReadKey();
+        }
+    }
+}
